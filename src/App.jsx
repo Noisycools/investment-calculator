7f4ec5e1b2a3da6aaec4c8fc3e55e8f01f4fb9ed
Aditya Nur Huda <adityanh1604@gmail.com>
2025-01-23 15:40:22 +0700

    chore: added strict mode and default input value

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,74 @@
-import { useState } from "react";
+import {useState} from "react";
 import ResultTable from "./components/ResultTable";
 import UserInput from "./components/UserInput/UserInput";
-import { calculateInvestmentResults } from "./util/investment";
-
-const RESULT_DATA = {
-  initialInvestment: 0,
-  annualInvestment: 0,
-  expectedReturn: 0,
-  duration: 0,
-};
+import {calculateInvestmentResults} from "./util/investment";
 
 function App() {
-  const [resultData, setResultData] = useState(RESULT_DATA);
+    const RESULT_DATA = {
+        initialInvestment: 1000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+    };
 
-  const annualData = calculateInvestmentResults(resultData);
-  const isValidData = resultData.duration >= 1;
+    const [resultData, setResultData] = useState(RESULT_DATA);
 
-  function inputChangeHandler(event) {
-    switch (event.target.id) {
-      case "initial":
-        setResultData((prevData) => {
-          return {
-            ...prevData,
-            initialInvestment: parseFloat(event.target.value),
-          };
-        });
-        break;
+    const annualData = calculateInvestmentResults(resultData);
+    const isValidData = resultData.duration >= 1;
 
-      case "annual":
-        setResultData((prevData) => {
-          return {
-            ...prevData,
-            annualInvestment: parseFloat(event.target.value),
-          };
-        });
-        break;
+    function inputChangeHandler(event) {
+        switch (event.target.id) {
+            case "initial":
+                setResultData((prevData) => {
+                    return {
+                        ...prevData,
+                        initialInvestment: parseFloat(event.target.value),
+                    };
+                });
+                break;
 
-      case "expected":
-        setResultData((prevData) => {
-          return {
-            ...prevData,
-            expectedReturn: parseFloat(event.target.value),
-          };
-        });
-        break;
+            case "annual":
+                setResultData((prevData) => {
+                    return {
+                        ...prevData,
+                        annualInvestment: parseFloat(event.target.value),
+                    };
+                });
+                break;
 
-      case "duration":
-        setResultData((prevData) => {
-          return {
-            ...prevData,
-            duration: parseFloat(event.target.value),
-          };
-        });
-        break;
+            case "expected":
+                setResultData((prevData) => {
+                    return {
+                        ...prevData,
+                        expectedReturn: parseFloat(event.target.value),
+                    };
+                });
+                break;
 
-      default:
-        break;
+            case "duration":
+                setResultData((prevData) => {
+                    return {
+                        ...prevData,
+                        duration: parseFloat(event.target.value),
+                    };
+                });
+                break;
+
+            default:
+                break;
+        }
     }
-  }
 
-  return (
-    <main>
-      <UserInput changeHandler={inputChangeHandler} />
-      {!isValidData ? (
-        <p className="center">Duration value should be greater than 0</p>
-      ) : (
-        <ResultTable annualData={annualData} />
-      )}
-    </main>
-  );
+    return (
+        <main>
+            <UserInput inputData={resultData} changeHandler={inputChangeHandler}/>
+            {!isValidData ? (
+                <p className="center">Duration value should be greater than 0</p>
+            ) : (
+                <ResultTable annualData={annualData}/>
+            )}
+        </main>
+    );
 }
 
 export default App;
--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,43 +1,47 @@
 import InputGroup from "./InputGroup";
 import InputLabel from "./InputLabel";
 
-function UserInput({ changeHandler }) {
-  return (
-    <section id="user-input">
-      <InputGroup>
-        <InputLabel
-          label="Initial Investment"
-          type="Number"
-          required
-          id="initial"
-          onChange={changeHandler}
-        />
-        <InputLabel
-          label="Annual Investment"
-          type="Number"
-          required
-          id="annual"
-          onChange={changeHandler}
-        />
-      </InputGroup>
-      <InputGroup>
-        <InputLabel
-          label="Expected Return"
-          type="Number"
-          required
-          id="expected"
-          onChange={changeHandler}
-        />
-        <InputLabel
-          label="Duration"
-          type="Number"
-          required
-          id="duration"
-          onChange={changeHandler}
-        />
-      </InputGroup>
-    </section>
-  );
+function UserInput({inputData, changeHandler}) {
+    return (
+        <section id="user-input">
+            <InputGroup>
+                <InputLabel
+                    label="Initial Investment"
+                    type="Number"
+                    required
+                    id="initial"
+                    value={inputData.initialInvestment}
+                    onChange={changeHandler}
+                />
+                <InputLabel
+                    label="Annual Investment"
+                    type="Number"
+                    required
+                    id="annual"
+                    value={inputData.annualInvestment}
+                    onChange={changeHandler}
+                />
+            </InputGroup>
+            <InputGroup>
+                <InputLabel
+                    label="Expected Return"
+                    type="Number"
+                    required
+                    id="expected"
+                    value={inputData.expectedReturn}
+                    onChange={changeHandler}
+                />
+                <InputLabel
+                    label="Duration"
+                    type="Number"
+                    required
+                    id="duration"
+                    value={inputData.duration}
+                    onChange={changeHandler}
+                />
+            </InputGroup>
+        </section>
+    );
 }
 
 export default UserInput;
